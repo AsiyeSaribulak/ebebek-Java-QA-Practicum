f792ad5477383e2798ceae6d517492c9c071e97c
Asiye Sarıbulak <5364542@ogrenci.bilecik.edu.tr>
2022-11-04 02:42:02 +0300

    java101 Harmonik Sayılar

---
+++ src/dongülerPratik1/HarmonikSayilar.java
@@ -0,0 +1,18 @@
+package dongülerPratik1;
+
+import java.util.Scanner;
+
+public class HarmonikSayilar {
+    public static void main(String[] args){
+        double num;
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter number:");
+        num=input.nextInt();
+        double harmonicSeries=0;
+        for(int i=1;i<=num;i++){
+            harmonicSeries+=1.0/i;
+        }
+        System.out.println("Result:"+harmonicSeries+1);
+    }
+}
+
