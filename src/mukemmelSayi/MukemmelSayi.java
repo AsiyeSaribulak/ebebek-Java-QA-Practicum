9b78db22d3333ee292c1cac6af6e8932b153ea7b
Asiye Sarıbulak <5364542@ogrenci.bilecik.edu.tr>
2022-11-09 00:10:49 +0300

    java101 perfect numbers find

---
+++ src/mukemmelSayi/MukemmelSayi.java
@@ -0,0 +1,25 @@
+package mukemmelSayi;
+
+import java.util.Scanner;
+
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        var in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = in.nextInt();
+        int sum = 0; //sum of all multiples
+
+        for(int i = 1; i < num; i++){
+            if(num % i == 0){
+                sum += i;
+            }
+        }
+
+        if(sum == num){
+            System.out.print(num+" is a perfect number.");
+        }else{
+            System.out.print(num+" is NOT a perfect number");
+        }
+    }
+}
+
